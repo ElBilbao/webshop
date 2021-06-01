bb70b45e28f0003b3047226f51a0f38c0a224f71
Rodrigo Bilbao <rbilbao98@gmail.com>
2021-06-01 00:24:22 -0500

    Delete from cart

--- server/routes/cart.js
+++ server/routes/cart.js
@@ -29,4 +29,31 @@ router.post("/:id", async (req, res) => {
     });
 });
 
+router.delete("/:id", async (req, res) => {
+  let user_id = req.params.id;
+  let product = req.body.product;
+
+  let cart = await cartModel.findOne({ userid: user_id });
+  if (!cart) res.status(404).end("That user has no cart");
+  let cart_content = cart.cart;
+  console.log(req.body);
+  let product_qty = cart.cart.get(product);
+
+  if (!product_qty)
+    res.status(403).end("That product was not found in the cart");
+  product_qty--;
+  if (product_qty == 0) {
+    cart_content.delete(product);
+  } else {
+    cart_content.set(product, product_qty);
+  }
+  cart
+    .save()
+    .then((user) => res.sendStatus(200))
+    .catch((err) => {
+      console.log(err);
+      res.status(503).end(`Could not update cart ${err}`);
+    });
+});
+
 module.exports = router;
